94521feac86058e2106c38abca3ac1278788e15c
ChristianCudby <christiancudby@hotmail.com>
2021-03-07 01:22:34 -0500

    adds connfigure block and some helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < Sinatra::Base
 
+    configure do
+        set(:views, 'app/views')
+        enable :sessions
+        set :session_secret, 'secret'
+    end
+
     get '/' do
         erb :welcome 
     end
+
+    helpers do
+
+        def current_user
+            @user = User.find_by(id: session[:user_id])
+        end
+
+        def redirect_if_not_logged_in
+            redirect '/login' unless current_user
+        end
+
+        def check_owner(obj)
+            obj && obj.user == current_user
+        end
     
 end
